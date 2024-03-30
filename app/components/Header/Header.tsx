5619c9c8e4a7afe5d8dab0f6333797e816404da1
Abijith26 <abimurugan.c@gmail.com>
2024-03-30 12:24:00 +0530

    Changed the styling of the header component

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Image from "next/image";
 import user from "../../../public/user_pic.png";
-import styles from "./Header.module.css";
+
 export default function Header() {
   return (
     <div className="w-full bg-black p-3 rounded flex justify-around items-center">
-      <p className="text-white text-2xl font-bold uppercase">films</p>
+      <p className="text-white text-2xl font-bold uppercase hover:text-red-700">
+        films
+      </p>
+
+      <form className="hidden sm:block">
+        <input
+          type="text"
+          placeholder="Search Movies..."
+          className="p-1 rounded-lg focus:outline-none focus:border-rose-500 border-2 border-transparent"
+        ></input>
+        <button className="text-white uppercase bg-red-700 ml-2 p-1 rounded-lg">
+          Search
+        </button>
+      </form>
 
       <ul className="text-gray-400 flex gap-6">
         <li>Home</li>
@@ -16,7 +29,7 @@ export default function Header() {
         src={user}
         width={100}
         height={100}
-        className="rounded-3xl w-9 h-9 hover:border-white"
+        className="rounded-3xl w-9 h-9 hover:opacity-50"
         placeholder="blur"
         alt="profile_pic"
       />
